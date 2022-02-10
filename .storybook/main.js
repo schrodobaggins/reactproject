ef99f9de24889f72981810d8bc6cbff55be984e6
antonisfk <antonisfkiaras@gmail.com>
2022-02-10 16:11:30 -0500

    fix: fixed folder structure and storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  }
 }
